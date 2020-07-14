fdfde207055307e8d4b55873644ad37d26fbf6ba
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2020-07-14 11:25:37 +0700

    Initial Commit

---
+++ Onboarding-SwiftUI-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onboarding-SwiftUI-Tutorial
+//
+//  Created by Arie May Wibowo on 14/07/20.
+//  Copyright © 2020 Arie May Wibowo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
